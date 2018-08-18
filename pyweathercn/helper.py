7220e8bc726adaf344c162778685359608837320
Benny 小土豆 <benny.think@gmail.com>
2018-08-18 10:20:56 +0800

    async non blocking

--- pyweathercn/helper.py
+++ pyweathercn/helper.py
@@ -3,31 +3,62 @@
 
 # pyweathercn - helper.py
 # 2018/5/22 16:32
-# 
+# For running up tornado
 
 __author__ = 'Benny <benny@bennythink.com>'
 
 import json
-
-import tornado.ioloop
-import tornado.web
-
+import socket
+from platform import uname
+from pyweathercn.__version__ import __version__
+from concurrent.futures import ThreadPoolExecutor
+from tornado import web, ioloop, httpserver, gen
+from tornado.concurrent import run_on_executor
 from pyweathercn.craw import make_json
 
+BANNER = r'''
 
-class MainHandler(tornado.web.RequestHandler):
+._           _   _. _|_ |_   _  ._ _ ._
+|_) \/ \/\/ (/_ (_|  |_ | | (/_ | (_ | |
+|   /
+
+A weather forecast library, version %s
+
+Written by %s
+''' % (__version__, __author__)
+
+
+class BaseHandler(web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.set_header("Content-Type", "application/json")
-        self.write(self.process(self))
+        help_msg = '''Welcome to pyweathercn!
+        There are two ways to interact with this RESTAPI.
+        The key parameter is city, and an optional parameter day.<br>
+        The first one is GET method, invoke:
+        <code>127.0.0.1:8888/weather?city=上海</code> - get full details
+        <code>127.0.0.1:8888/weather?city=上海&day=2</code> - get 2 days details
+        The second one is POST method, invoke <code>127.0.0.1:8888/weather</code>
+         with url-encoded form as above. Post JSON is also supported.
+        '''
+        base = '''<!DOCTYPE html><html><head><title>Welcome to pyweathercn!</title></head>
+        <body>%s</body></html>'''
+        self.write(base % (BANNER + '<br>' + help_msg).replace('\n', '<br>'))
 
-    def post(self):
-        self.set_header("Content-Type", "application/json")
-        self.write(self.process(self))
 
-    @staticmethod
-    def process(self):
+class WeatherHandler(BaseHandler):
+    executor = ThreadPoolExecutor(max_workers=20)
+
+    @run_on_executor
+    def run_request(self):
+        """
+        sign and return
+        :return: hex and raw request in XML
+        """
         # get parameter
-
         city = self.get_argument('city', None)
         day = self.get_argument('day', None)
 
@@ -46,21 +77,46 @@ class MainHandler(tornado.web.RequestHandler):
         else:
             return json.dumps(make_json(city))
 
+    @gen.coroutine
+    def get(self):
+        self.set_header("Content-Type", "application/json")
+        res = yield self.run_request()
+        self.write(res)
 
-def make_app():
-    return tornado.web.Application([
-        (r"/weather", MainHandler),
-    ])
+    @gen.coroutine
+    def post(self):
+        self.set_header("Content-Type", "application/json")
+        res = yield self.run_request()
+        self.write(res)
 
 
-def run_server(port, host, **kwargs):
-    app = make_app()
-    app.listen(port, host, **kwargs)
-    print('%s running on %s' % (host, port))
-    tornado.ioloop.IOLoop.current().start()
+def get_host_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def run_server(port=8888, host='0.0.0.0', **kwargs):
+    handlers = [(r'/weather', WeatherHandler), (r'/', MainHandler)]
+    application = web.Application(handlers)
+    tornado_server = httpserver.HTTPServer(application)
+    tornado_server.bind(port, host)
+
+    if uname()[0] == 'Windows':
+        tornado_server.start()
+    else:
+        tornado_server.start(None)
+
+    try:
+        print(BANNER)
+        print('Server is running on http://%s:%s' % (get_host_ip(), port))
+        ioloop.IOLoop.instance().current().start()
+    except KeyboardInterrupt:
+        ioloop.IOLoop.instance().stop()
+        print('"Ctrl+C" received, exiting.\n')
 
 
 if __name__ == "__main__":
-    run_server('8888', 'api.serversan.date', ssl_options={
-        "certfile": "fullchain.pem",
-        "keyfile": "privkey.pem"})
+    run_server()
